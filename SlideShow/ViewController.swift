4d71c4600066959ed88834908d7f6ad77e4972ed
e6420 <e6420@ioss-macbook-air.local>
2018-11-10 16:25:39 +0530

    Initial Commit

---
+++ SlideShow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideShow
+//
+//  Created by OJASWI DEWANGAN on 10/11/18.
+//  Copyright © 2018 Thyrocare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
